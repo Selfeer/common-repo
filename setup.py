81627bebf02adfda1d7c8617c34b15554b9b2a63
selfe <davit.mnatobishvili.1@btu.edu.ge>
2022-11-01 14:16:39 +0400

    First commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='common-repo',
+    version='1.1',
+    packages=['front-end-package'],
+    url='',
+    license='',
+    author='selfe',
+    author_email='selfeers@gmail.com',
+    description=''
+)
