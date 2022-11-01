6778e92b5e1dbf1e4c8498ec41265af91dc9553e
selfe <davit.mnatobishvili.1@btu.edu.ge>
2022-11-01 20:36:18 +0400

    First commit

--- front-end-package/page.py
+++ front-end-package/page.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
 
 def open_page():
--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='front-end-package',
-    version='1.1',
+    name='fep',
+    version='0.0.1',
     packages=['front-end-package'],
     url='https://github.com/Selfeer/common-repo',
     license='',
     author='selfe',
-    author_email='selfeers@gmail.com',
-    description='',
+    author_email='',
+    description=''
 )
