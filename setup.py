9252b10df2587b9ff009ffef94442b61c4a41026
selfe <davit.mnatobishvili.1@btu.edu.ge>
2022-11-15 11:50:40 +0400

    First commit

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='fep',
-    version='0.0.1',
-    packages=['front-end-package'],
+    version='0.1',
+    packages=['fep'],
     url='https://github.com/Selfeer/common-repo',
     license='',
     author='selfe',
