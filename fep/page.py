a42c680f17fa4e857fd51c745143c037c17f369c
selfe <davit.mnatobishvili.1@btu.edu.ge>
2022-11-15 14:26:36 +0400

    First commit

--- fep/page.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-
-
-def open_page():
-    driver = webdriver.Chrome('chromedriver.exe')
-    driver.get("https://www.google.com")
-    driver.close()
--- setup.py
+++ setup.py
@@ -4,10 +4,9 @@ setup(
     name='fep',
     version='0.4',
     packages=['fep'],
-    package_data={'fep': ['Robot/test.robot']},
-    url='https://github.com/Selfeer/common-repo',
     license='',
     author='selfe',
     author_email='',
-    description=''
+    description='',
+    include_package_data=True
 )
